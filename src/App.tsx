c633e5125c59a5e8d1dac084a4462114a4023d22
Rohit Ramesh <rrohitramesh710@gmail.com>
2025-06-17 13:52:18 +0200

    Minor front end changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ import './App.css'
 function App() {
   return (
     <AppKitProvider>
-      <div className="app-container">
-        <h1>Farcaster Mini App</h1>
-        <div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        padding: '2rem',
+        margin: '0 auto'
+      }}>
+        <h1 style={{ color: 'white', textAlign: 'center', marginBottom: '2rem' }}>
+          Farcaster Mini App
+        </h1>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
           <appkit-button />
         </div>
       </div>
