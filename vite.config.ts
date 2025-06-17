b70876f3127e9c79b76aae04784b1f88a70059e5
Rohit Ramesh <rrohitramesh710@gmail.com>
2025-06-17 14:20:40 +0200

    Removing meta tag

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "frame-ancestors 'self' https://appkit-farcaster-mini-app.vercel.app/ http://localhost:* https://*.pages.dev https://*.vercel.app https://*.ngrok-free.app https://secure-mobile.walletconnect.com https://secure-mobile.walletconnect.org"
+    }
+  }
 })
